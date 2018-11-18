5d2c554030b299a0323b430fb2311b00e2e49a99
falgs@iscte-iul.pt <Filipe@dynamic-eduroam.217.11.17.172.rede>
2018-11-18 20:47:48 +0000

    Criação classe TwitterAppTest para realizar Junit do Twitter

---
+++ src/test/java/principal/TwitterAppTest.java
@@ -0,0 +1,24 @@
+package principal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TwitterAppTest {
+
+	@Test
+	public void testRunTwitter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteTwitterXML() {
+		fail("Not yet implemented");
+	}
+
+}
