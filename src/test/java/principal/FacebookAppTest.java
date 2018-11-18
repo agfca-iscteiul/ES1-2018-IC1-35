7341d64f6b21422c7ae4f8f1d6802a954c40963f
falgs@iscte-iul.pt <Filipe@dynamic-eduroam.217.11.17.172.rede>
2018-11-18 18:50:48 +0000

    Criação da classe testes unitários utilizando testes automáticos

---
+++ src/test/java/principal/FacebookAppTest.java
@@ -0,0 +1,29 @@
+package principal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FacebookAppTest {
+
+	@Test
+	public void testFacebookApp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunFacebook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteFacebookXML() {
+		fail("Not yet implemented");
+	}
+
+}
