9c9c1e187d50efe214e3ccf4094b96fd113be77f
André <agfca@iscte-iul.pt>
2018-11-30 15:27:27 +0000

    A classe XMLEditor já cria e apaga ficheiros XML

--- src/main/java/principal/XMLEditor.java
+++ src/main/java/principal/XMLEditor.java
@@ -1,5 +1,25 @@
 package principal;
 
-public class XMLEditor {
+import java.io.File;
 
+public class XMLEditor {
+	
+	FacebookApp facebook = new FacebookApp();
+	TwitterApp twitter = new TwitterApp();
+	MailApp outlook= new MailApp();
+	
+	public void createXMLFile() {
+		facebook.writeFacebookXML();
+		twitter.writeTwitterXML();
+		outlook.writeMailXML();
+	}
+	
+	public void removeXMLFile() {
+		File data = new File("config.xml");
+		if(data.delete()) {
+			System.out.println("O ficheiro XML foi removido");
+		}else{
+			System.out.println("Não existe ficheiro XML para ser removido");
+		}
+	}
 }
