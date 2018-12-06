c1cfa73a17b04f3c278dee2babef257f53e30056
falgs@iscte-iul.pt <Filipe@mbp-de-beatriz.home>
2018-12-06 23:34:21 +0000

    implementação das funções na classe facebook

--- src/main/java/principal/FacebookApp.java
+++ src/main/java/principal/FacebookApp.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -26,32 +27,37 @@ import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.FacebookClient.AccessToken;
+import com.restfb.Parameter;
 import com.restfb.Version;
+import com.restfb.types.FacebookType;
+import com.restfb.types.Group;
 import com.restfb.types.Post;
 import com.restfb.types.User;
 
 public class FacebookApp {
 	private ArrayList<FacebookInfo> lista = new ArrayList<FacebookInfo>();
+	private String accessToken;
 
 	public FacebookApp() {
 
-	} 
-	
+	}
+
 	/**
-	 * Aceder ao Token e extender o tempo do mesmo através da API RestFB, imprimir os post da timeline do user com a palavra "ISCTE" e adiciona-los ao array como FacebookInfo
+	 * Aceder ao Token e extender o tempo do mesmo através da API RestFB, imprimir
+	 * os post da timeline do user com a palavra "ISCTE" e adiciona-los ao array
+	 * como FacebookInfo
 	 */
 	public void runFacebook() {
 		try {
-			String accessToken = "EAAffZC9Xl8dEBAPcqTRMVZB2wT1d8g98YUB9TbUAIVXwRSOFLLmJxNs00oLO0TLlMf7XnempwnZBCP9lfh6t0zL7nbkvFlZABPC60TXDprR2Ty3UY9jm6gil4FtqXNv6HnPooBXND2XXFAODkWYZA1XmhZAPe950MgJ3yHAZB7jHzf3dHb4ZCqCgtYqkZBjRCJtTiJfJVakmt0hCksQJ9F6kW";
+			accessToken = "EAAffZC9Xl8dEBAPWiu23ZAnlwDEIFcl9olxZBPiZCHdZBp4mSgy155GSHL2C6f45ixXrQUcNOdpy4rscyQur6lIEZBZBOmPYZA5B0DkfxZCbm0fhkWWP8BlTdZBsBb5qi5fibRiZAiymk2GHTocR3PxkwJ8YnetpiZCCUwEnxZCaXiKqB9QMc8lFEMhgg9uAWRzqqG1pDtSAInG3nCcPqcG14nja6";
 			FacebookClient fbClient = new DefaultFacebookClient(accessToken, Version.VERSION_2_12);
 			User me = fbClient.fetchObject("me", User.class);
-			
+
 			// Extender o tempo do token
 			AccessToken exAcessToken = fbClient.obtainExtendedAccessToken("2216614735245777",
 					"47baee1d3c2b6366f7212a3dfa403dd3");
 			System.out.println(exAcessToken.getAccessToken() + exAcessToken.getExpires());
 
-			
 			// Imprimir os post da timeline do user
 			Connection<Post> result = fbClient.fetchConnection("me/feed", Post.class);
 
@@ -63,21 +69,58 @@ public class FacebookApp {
 					}
 				}
 			}
+			/*
+			// Imprimir os post dos grupos dos quais somos admin
+			Scanner input = new Scanner(System.in);
+
+			Connection<Group> groupFeed = fbClient.fetchConnection("me/groups", Group.class);
+
+			for (List<Group> page : groupFeed) {
+				for (Group aGroup : page) {
+					System.out.println("Quer obter post do grupo " + aGroup.getName() + "? Sim/Nao");
+					String ans = input.nextLine();
+					if (ans.equals("Sim")) {
+						System.out.println("fb.com/" + aGroup.getId());
+						System.out.println("Post deste grupo");
+						Connection<Post> postFeed = fbClient.fetchConnection(aGroup.getId()+"/feed", Post.class);
+						for (List<Post> postPage : postFeed) {
+							for (Post aPost : postPage) {
+								System.out.println("Mensagem: \n");
+								System.out.println(aPost.getMessage());
+							}
+						}
+					}
+				}
+			}*/
+			
+			//Postar na timeline de grupos aos quais somos admin
+			FacebookType response = fbClient.publish("372019400036311/feed", FacebookType.class, Parameter.with("message", "testetesteteste"));
+			//Se quisermos acrescentar um link
+			//FacebookType response = fbClient.publish("id grupo/feed", FacebookType.class, Parameter.with("message", "testetesteteste"), Parameter.with("link", "www.google.pt"));
+			//Para publicar uma imagem
+			// FileInputStream file = new FileInputStream(new File(local onde imagem está no disco));
+			// FacebookType response = fbClient.publish("id grupo/feed", FacebookType.class, binaryAttachment.with("nomeimg", file), Parameter.with("message", "testetesteteste"));
+			// FileInputStream file = new FileInputStream(new File(local onde video está no disco));
+			// FacebookType response = fbClient.publish("id grupo/feed", FacebookType.class, binaryAttachment.with("nomevideo", file), Parameter.with("message", "testetesteteste"));
+			
+			
+			System.out.println("fb.com/"+response.getId());
+
 		} catch (Exception e) {
-			XMLEditor xml=new XMLEditor();
-			xml.readFromXML();
-			lista.addAll(xml.getListaFacebook());
+			//XMLEditor xml = new XMLEditor();
+			//xml.readFromXML();
+			//lista.addAll(xml.getListaFacebook());
 		}
 	}
-	
-	
-	public ArrayList<FacebookInfo> getListPost(){
+
+	public ArrayList<FacebookInfo> getListPost() {
 		return lista;
-		
+
 	}
-	
+
 	/**
 	 * Transformar as lista de FacebookInfo para listas AbstractInfo
+	 * 
 	 * @return lista AbstractInfo com os post
 	 */
 	public ArrayList<AbstractInfo> getList() {
@@ -87,89 +130,88 @@ public class FacebookApp {
 		}
 		return listaaux;
 	}
-	
-	
+
 	/**
 	 * Escrever no ficheiro XML todos os post do facebook
 	 */
 	// Escrever no ficheiro XML
-		public void writeFacebookXML() {
-			File datebase = new File("config.xml");
-			if (datebase.exists()) {
-				System.out.println("A file já existe");
-				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-				DocumentBuilder dBuilder;
-				try {
-					dBuilder = dbFactory.newDocumentBuilder();
-					Document doc = dBuilder.parse(datebase); 
-					Node root = doc.getDocumentElement();
-					root.normalize();
-					Element tree = doc.createElement("Serviço");
-					root.appendChild(tree);
-					tree.setAttribute("Plataforma", "Facebook");
-					String autor, post;
-					Date data;
-					for (FacebookInfo tdados : lista) {
-						autor = tdados.getAutor();
-						data = tdados.getData();
-						post = tdados.getPost();
-						Element tweet = doc.createElement("Post");
-						tweet.setAttribute("Autor", autor);
-						tweet.setAttribute("Data", data.toString());
-						tweet.setTextContent(post);
-						tree.appendChild(tweet);
-					}
-					System.out.println("\nSave XML document.");
-					Transformer transformer = TransformerFactory.newInstance().newTransformer();
-					transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-					transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-					DOMSource source = new DOMSource(doc);
-					FileOutputStream foutstream=new FileOutputStream("config.xml");
-					StreamResult result = new StreamResult(foutstream);
-					transformer.transform(source, result);
-					StreamResult consoleResult = new StreamResult(System.out);
-					transformer.transform(source, consoleResult);
-					foutstream.close();
-				} catch (ParserConfigurationException | SAXException | TransformerFactoryConfigurationError
-						| TransformerException | IOException e) {
-					e.printStackTrace();
+	public void writeFacebookXML() {
+		File datebase = new File("config.xml");
+		if (datebase.exists()) {
+			System.out.println("A file já existe");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder;
+			try {
+				dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(datebase);
+				Node root = doc.getDocumentElement();
+				root.normalize();
+				Element tree = doc.createElement("Serviço");
+				root.appendChild(tree);
+				tree.setAttribute("Plataforma", "Facebook");
+				String autor, post;
+				Date data;
+				for (FacebookInfo tdados : lista) {
+					autor = tdados.getAutor();
+					data = tdados.getData();
+					post = tdados.getPost();
+					Element tweet = doc.createElement("Post");
+					tweet.setAttribute("Autor", autor);
+					tweet.setAttribute("Data", data.toString());
+					tweet.setTextContent(post);
+					tree.appendChild(tweet);
 				}
+				System.out.println("\nSave XML document.");
+				Transformer transformer = TransformerFactory.newInstance().newTransformer();
+				transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+				transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+				DOMSource source = new DOMSource(doc);
+				FileOutputStream foutstream = new FileOutputStream("config.xml");
+				StreamResult result = new StreamResult(foutstream);
+				transformer.transform(source, result);
+				StreamResult consoleResult = new StreamResult(System.out);
+				transformer.transform(source, consoleResult);
+				foutstream.close();
+			} catch (ParserConfigurationException | SAXException | TransformerFactoryConfigurationError
+					| TransformerException | IOException e) {
+				e.printStackTrace();
+			}
 
-			} else {
-				try {
-					System.out.println("A file não existe");
-					DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-					DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-					Document doc = dBuilder.newDocument();
-					Element rootElement = doc.createElement("Serviços");
-					doc.appendChild(rootElement);
-					Element tree = doc.createElement("Serviço");
-					rootElement.appendChild(tree);
-					tree.setAttribute("Plataforma", "Facebook");
-					String autor, post;
-					Date data;
-					for (FacebookInfo tdados : lista) {
-						autor = tdados.getAutor();
-						data = tdados.getData();
-						post = tdados.getPost();
-						Element tweet = doc.createElement("Post");
-						tweet.setAttribute("Autor", autor);
-						tweet.setAttribute("Data", data.toString());
-						tweet.setTextContent(post);
-						tree.appendChild(tweet);
-					}
-					System.out.println("\nSave XML document.");
-					Transformer transformer = TransformerFactory.newInstance().newTransformer();
-					transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-					transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-					DOMSource source = new DOMSource(doc);
-					StreamResult result = new StreamResult(new File("config.xml"));
-					transformer.transform(source, result);
-					StreamResult consoleResult = new StreamResult(System.out);
-					transformer.transform(source, consoleResult);
-				} catch (ParserConfigurationException | TransformerFactoryConfigurationError | TransformerException e) {
-					e.printStackTrace();
+		} else {
+			try {
+				System.out.println("A file não existe");
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.newDocument();
+				Element rootElement = doc.createElement("Serviços");
+				doc.appendChild(rootElement);
+				Element tree = doc.createElement("Serviço");
+				rootElement.appendChild(tree);
+				tree.setAttribute("Plataforma", "Facebook");
+				String autor, post;
+				Date data;
+				for (FacebookInfo tdados : lista) {
+					autor = tdados.getAutor();
+					data = tdados.getData();
+					post = tdados.getPost();
+					Element tweet = doc.createElement("Post");
+					tweet.setAttribute("Autor", autor);
+					tweet.setAttribute("Data", data.toString());
+					tweet.setTextContent(post);
+					tree.appendChild(tweet);
 				}
+				System.out.println("\nSave XML document.");
+				Transformer transformer = TransformerFactory.newInstance().newTransformer();
+				transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+				transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+				DOMSource source = new DOMSource(doc);
+				StreamResult result = new StreamResult(new File("config.xml"));
+				transformer.transform(source, result);
+				StreamResult consoleResult = new StreamResult(System.out);
+				transformer.transform(source, consoleResult);
+			} catch (ParserConfigurationException | TransformerFactoryConfigurationError | TransformerException e) {
+				e.printStackTrace();
 			}
 		}
+	}
 }
